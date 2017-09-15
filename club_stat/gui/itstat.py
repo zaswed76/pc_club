8c7bab9de015f6238971b562ecba7b9ac1843171
Cassa <zaswed76@gmail.com>
2017-09-15 23:52:26 +0300

    create struct

--- club_stat/gui/itstat.py
+++ club_stat/gui/itstat.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from PyQt5 import QtWidgets, uic
+from club_stat import webdriver, config, club
 
 ui_pth = r"C:\Users\Cassa\Desktop\Serg\project\pc_club\club_stat\gui\ui\itstat.ui"
 
@@ -11,7 +12,22 @@ class ItStat(QtWidgets.QMainWindow):
         self.center = QtWidgets.QFrame()
         self.setCentralWidget(self.center)
         self.form = uic.loadUi(ui_pth, self.center)
-        # self.w.setFixedSize(500, 250)
+
+        self.form.start.clicked.connect(
+            self.start)
+
+
+
+    def start(self):
+        adr = self.form.adress.text()
+        login_id = 'enter_login'
+        password_id = 'enter_password'
+        submit_name = 'but_m'
+        login = self.form.login.text()
+        password = self.form.password.text()
+        web = webdriver.WebDriver(adr, webdriver.WebDriver.Firefox)
+        web.log_in(login_id, password_id, submit_name,
+                        login, password)
 
 
 
--- club_stat/main.py
+++ club_stat/main.py
@@ -2,36 +2,19 @@ import sys
 from PyQt5 import QtWidgets
 
 
-from club_stat import webdriver, config, club
+
 from club_stat.gui.itstat import ItStat
 
 adr = "http://adminold.itland.enes.tech/index.php/map"
 
-login_id = 'enter_login'
-password_id = 'enter_password'
-submit_name = 'but_m'
-login = ""
-password = ""
 
 
 
 def main():
-
     app = QtWidgets.QApplication(sys.argv)
     app.setStyleSheet(open('css/style.css', "r").read())
     gui = ItStat()
     gui.show()
-
-    login_id = 'enter_login'
-    password_id = 'enter_password'
-    submit_name = 'but_m'
-    login = ""
-    password = ""
-    print(gui.form.adress)
-
-    # web = webdriver.WebDriver(adr, webdriver.WebDriver.Firefox)
-    # web.log_in(login_id, password_id, submit_name,
-    #                     login, password)
     sys.exit(app.exec_())
 
 
