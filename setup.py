a059f10f1e3aeef8cfc60b1710bde79c69046200
serg <zaswed76@gmail.com>
2017-09-16 13:29:22 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         include_package_data=True,
         long_description=open(
                 join(dirname(__file__), 'README.rst')).read(),
-        install_requires=["PyQt5"],
+        install_requires=["PyQt5", "selenium"],
         entry_points={
             'console_scripts':
                 ['itstat = club_stat.main:Main']
