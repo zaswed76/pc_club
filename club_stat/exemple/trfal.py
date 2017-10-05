ea4a425f0c093be2e2113b50008a41e5eda58e28
Cassa <zaswed76@gmail.com>
2017-10-05 16:54:05 +0300

    create out_form

--- club_stat/exemple/trfal.py
+++ club_stat/exemple/trfal.py
@@ -1,30 +1,37 @@
-lst = [(1, 0, 1), (1, 3, 2), (1, 3, 3), (2, 0, 1), (2, 0, 2),
-       (2, 3, 3), (2, 3, 4), (3, 0, 1), (3, 0, 2)]
+
+import collections
+
+class State(collections.Iterator):
+    s = ["dt",
+    "data_time",
+    "mhour",
+    "mminute",
+    "club",
+    "load",
+    "taken",
+    "free",
+    "guest",
+    "resident",
+    "admin",
+    "workers",
+    "school",
+    "visitor"]
+    def __init__(self, state):
+        self.value = 0
+        self.stop = len(self.s)
+        self.state = {}
+        for n, v in zip(self.s, state):
+            self.state[n] = v
 
 
-class Akm(list):
-    def __init__(self):
-        super().__init__()
-
-    def __contains__(self, item):
-        for line in self:
-            if item[0] == line[0] and item[1] == line[1]:
-                return True
+    def __next__(self):
+        if self.value == self.stop:
+            raise  StopIteration
+        st = self.s[self.value]
+        self.value += 1
+        return st
 
 
-def sort_seq(lst):
-    akm = Akm()
-    for line in lst:
-        if not (line[0], line[1]) in akm:
-            akm.append(line)
-    return akm
 
-
-def sort_seq2(lst):
-    akm = Akm()
-    akm = [x for x in lst if not x[0] in akm]
-    return akm
-
-
-if __name__ == '__main__':
-    print(sort_seq(lst))
+s = State(range(13))
+print(s.state["dt"])
