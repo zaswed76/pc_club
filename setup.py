aa7f5d6c1d78ad2a5ab3e26b9364da0c6aa8e7ac
Cassa <zaswed76@gmail.com>
2017-10-15 21:54:18 +0300

    sql_keeper fix

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         install_requires=["PyQt5", "selenium"],
         entry_points={
             'console_scripts':
-                ['itstat = club_stat.main:Main']
+                ['itstat2 = club_stat.main:Main']
         }
 
 )
