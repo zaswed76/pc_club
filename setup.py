8e0318f74b27cda8d9fb42c177d07a4f18284b8f
Cassa <zaswed76@gmail.com>
2017-11-27 10:41:35 +0200

    driver fix

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
         include_package_data=True,
         long_description=open(
                 join(dirname(__file__), 'README.rst')).read(),
-        install_requires=[],
-        # install_requires=["PyQt5", "selenium"],
+
+        install_requires=["PyQt5", "selenium"],
         entry_points={
             'console_scripts':
                 ['itstat = club_stat.main:Main']
