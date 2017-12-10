6c2e0093a4ae6406bea0d43be34f17ac2eecc857
serg <zaswed76@gmail.com>
2017-12-10 21:34:52 +0200

    qas

--- club_stat/Map.py
+++ club_stat/Map.py
@@ -1,6 +1,8 @@
 
+#
+# pro_zone_id = {"pc111254": {"data-id":"111253", "data-ip":"172.16.11.16", "data-mac":"9c:5c:8e:97:6d:e0",
+#                             }
+#
+#
 
-class Map:
-    def __init__(self):
-        self.pro_zone_id = ["pc111254", "pc111253", "pc111252",
-                            "pc111251", "pc111250", "pc111240"]
+les_id_pro_zone = ["pc111254", "pc111253", "pc111252", "pc111251", "pc111250", "pc111240"]
---
+++ club_stat/lib/map_creator_config.py
@@ -0,0 +1,59 @@
+import os
+from club_stat.webdriver import WebDriver
+from bs4 import BeautifulSoup
+
+class MapCreator(WebDriver):
+    def __init__(self, adr, driver_pth, binary_pth):
+        super().__init__(adr, driver_pth, binary_pth, )
+
+    def get_table(self):
+        text = self.browser.page_source
+        soup = BeautifulSoup(text)
+        table = soup.find('table', id="map")
+        for tr in table.find_all('tr'):
+            for d in tr:
+                tag = d.findAll("span")
+                if tag:
+                    print(tag[0]["id"])
+                    print("----------------------")
+            # for i in tr:
+            #     for span_tag in i.findAll('span'):
+            #         span_tag.replace_with('')
+            #         print(i)
+                    # print(span[0].extract())
+                    # print(span[0].replace_with(""))
+                    # new_tag = soup.new_tag("")
+                    # new_tag.string = "example.net"
+                    # span[0].i.replace_with(new_tag)
+                    # print(span[0].find(True))
+                    # r = [x.extract() for x in i.findAll("span")]
+                    # print("------------------")
+                # except AttributeError:
+                #     pass
+                # print("-----------------")
+                # # r = i.findAll("span", id="pc111253")
+                # # if r:
+                # #     print(r)
+                # #     print("------------------------")
+
+from club_stat import pth, config
+import time
+cfg = config.load(pth.CONFIG_PATH)
+adr = "http://adminold.itland.enes.tech/index.php/map"
+login_id = 'enter_login'
+password_id = 'enter_password'
+submit_name = 'but_m'
+login = "zaswed"
+password = "fasadAQ9"
+driver_pth = os.path.join(pth.DRIVERS_DIR,
+                              cfg["driver"])
+binary_pth = os.path.abspath(cfg["binary_browser_pth"])
+driver = MapCreator(adr, driver_pth, binary_pth)
+driver.log_in(login_id, password_id, submit_name,
+                        login, password)
+time.sleep(1)
+
+driver.select_club("4")
+time.sleep(1)
+driver.get_table()
+driver.close()
